e0aa890975eb06a32918d7eb50401c9754ad6638
akirakimura1st <girlsbandparty1th@gmail.com>
2019-03-16 13:21:52 +0000

    [update]

--- db/fixtures/01_user.rb
+++ db/fixtures/01_user.rb
@@ -4,10 +4,10 @@ user = User.seed do |s|
     s.encrypted_password = "test123"
     s.phone_number = "080-0000-0000"
     s.registration = true
-    s.kanji_last = "木村"
-    s.kanji_first = "彰良"
-    s.katakana_last = "キムラ"
-    s.katakana_first = "アキラ"
+    s.kanji_last = "管理"
+    s.kanji_first = "使用者"
+    s.katakana_last = "カンリ"
+    s.katakana_first = "ユーザー"
     s.admin_user = true
 end
 
